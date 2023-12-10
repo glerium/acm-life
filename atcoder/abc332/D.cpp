7fcd7ab2067040e1f234e3525767bc2d03d0e0f6
glerium <970656328@qq.com>
2023-12-10 20:31:56 +0800

    Add file

--- atcoder/abc332/D.cpp
+++ atcoder/abc332/D.cpp
@@ -1,24 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-int n,m,a[10][10],b[10][10],r[10],c[10];
-vector<int> rows[10], cols[10];
+int n,m;
+ll a[10][10],b[10][10],r[10],c[10];
 int main() {
     cin>>n>>m;
     for(int i=1;i<=n;i++) for(int j=1;j<=m;j++) cin>>a[i][j];
     for(int i=1;i<=n;i++) for(int j=1;j<=m;j++) cin>>b[i][j];
     for(int i=1;i<=n;i++) {
-        vector<int> t;
-        for(int j=1;j<=m;j++) t.push_back(b[i][j]);
-        sort(t.begin(), t.end());
-        rows[i]=t;
+        ll tt=0;
+        for(int j=1;j<=m;j++) tt=(tt*)
     }
-    for(int j=1;j<=m;j++) {
-        vector<int> t;
-        for(int i=1;i<=n;i++) t.push_back(b[i][j]);
-        sort(t.begin(), t.end());
-        cols[j]=t;
-    }
-    
     return 0;
 }
